beea549221db1c2bb2a6f63259334d866b31ae29
troyh <troyheizer@gmail.com>
2023-11-14 10:53:27 -0500

    Fourth commit wrote create funcion for movie

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace MovieDB.Pages
+
+{
+    public partial class Index
+    {
+        public bool ShowCreate { get; set; }
+    }
+    protected override async Task OnInitializedAsync()
+    {
+        ShowCreate = false;
+    }
+
+    private MovieDataContext? _context;
+    public Movie? New { get; Movieset; }
+
+    public void ShowCreateForm()
+    {
+        NewMovie = new Movie();
+        ShowCreate = true;
+    }
+    public async Task CreateNewMovie()
+    { _context ??= await MovieDataContextFactory.CreateDbContextAsync();
+
+        if (NewMovie is not null)
+        {
+            _context?.Movies.Add(NewMovie);
+            _context?.SaveChangesAsync();
+        }
+        ShowCreate = false;
+    }
+}
+
+
