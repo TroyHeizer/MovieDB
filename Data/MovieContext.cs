645feacdcff76bc42f2764ea0c8c5115e7a5d11b
troyh <troyheizer@gmail.com>
2023-11-14 10:17:36 -0500

    Third Seeded database

--- Data/MovieContext.cs
+++ Data/MovieContext.cs
@@ -17,5 +17,45 @@ namespace MovieDB.Data
         }
 
         public DbSet<Movie> Movies { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .ToTable("Movie");
+
+                modelBuilder.Entity<Movie>()
+                    .HasData(
+                new Movie { Id = 1, MovieName = "Sherlock Holmes and the Secret Weapon", MovieGenre = "Crime|Mystery|Thriller", ReleaseDate = "09/29/2004"},
+                new Movie { Id = 2, MovieName = "Lady and the Tramp", MovieGenre = "Animation|Children|Comedy|Romance", ReleaseDate = "09/05/1970"},
+                new Movie { Id = 3, MovieName = "Collision: Christopher Hitchens vs. Douglas Wilson", MovieGenre = "Documentary", ReleaseDate = "12/04/1987"},
+                new Movie { Id = 4, MovieName = "Cooking Up Dreams (De ollas y sueños)", MovieGenre = "Documentary", ReleaseDate = "12/14/2013"},
+                new Movie { Id = 5, MovieName = "Merry Madagascar", MovieGenre = "Animation", ReleaseDate = "04/13/1953"},
+                new Movie { Id = 6, MovieName = "Sleeper", MovieGenre = "Comedy|Sci-Fi", ReleaseDate = "07/02/2022"},
+                new Movie { Id = 7, MovieName = "Article 99", MovieGenre = "Comedy|Drama", ReleaseDate = "05/21/1974"},
+                new Movie { Id = 8, MovieName = "Battle in Outer Space", MovieGenre = "Adventure|Sci-Fi", ReleaseDate = "10/06/1990"},
+                new Movie { Id = 9, MovieName = "The Pirates of Blood River", MovieGenre = "Action|Adventure|Comedy", ReleaseDate = "05/11/2009"},
+                new Movie { Id = 10, MovieName = "Fun Size", MovieGenre = "Comedy", ReleaseDate = "06/30/1966"},
+                new Movie { Id = 11, MovieName = "Lenny", MovieGenre = "Drama", ReleaseDate = "11/19/2011"},
+                new Movie { Id = 12, MovieName = "Sonny", MovieGenre = "Crime|Drama", ReleaseDate = "06/12/1956"},
+                new Movie { Id = 13, MovieName = "The Loft", MovieGenre = "Thriller", ReleaseDate = "06/11/2001"},
+                new Movie { Id = 14, MovieName = "Sunshine", MovieGenre = "Adventure|Drama|Sci-Fi|Thriller", ReleaseDate = "11/16/1978"},
+                new Movie { Id = 15, MovieName = "One Crazy Summer", MovieGenre = "Comedy", ReleaseDate = "07/15/2000"},
+                new Movie { Id = 16, MovieName = "Prophet's Prey", MovieGenre = "Crime|Documentary", ReleaseDate = "11/05/1973"},
+                new Movie { Id = 17, MovieName = "Confidentially Yours (Vivement dimanche!)", MovieGenre = "Comedy|Crime|Mystery|Thriller", ReleaseDate = "03/24/1958"},
+                new Movie { Id = 18, MovieName = "Librarian: Quest for the Spear", MovieGenre = "Action|Adventure|Comedy|Fantasy|Romance", ReleaseDate = "12/05/2004"},
+                new Movie { Id = 19, MovieName = "The Power and the Glory", MovieGenre = "Drama", ReleaseDate = "03/30/1950"},
+                new Movie { Id = 20, MovieName = "Lupin the 3rd", MovieGenre = "Action|Adventure|Comedy", ReleaseDate = "10/08/1966"},
+                new Movie { Id = 21, MovieName = "Dream (Bi-mong)", MovieGenre = "Drama|Fantasy|Mystery|Romance", ReleaseDate = "07/04/1986"},
+                new Movie { Id = 22, MovieName = "Neds", MovieGenre = "Drama", ReleaseDate = "02/17/2020"},
+                new Movie { Id = 23, MovieName = "Bodyguard", MovieGenre = "Drama|Romance|Thriller", ReleaseDate = "11/25/1992"},
+                new Movie { Id = 24, MovieName = "Austin High", MovieGenre = "Comedy", ReleaseDate = "05/07/2010"},
+                new Movie { Id = 25, MovieName = "Vision (Vision - Aus dem Leben der Hildegard von Bingen)", MovieGenre = "Drama", ReleaseDate = "10/12/2013"},
+                new Movie { Id = 26, MovieName = "Reconstruction (Anaparastasi)", MovieGenre = "Crime|Drama", ReleaseDate = "03/19/2022"},
+                new Movie { Id = 27, MovieName = "Fever Pitch", MovieGenre = "Comedy|Romance", ReleaseDate = "09/10/1973"},
+                new Movie { Id = 28, MovieName = "Holocaust", MovieGenre = "Drama|Romance|War", ReleaseDate = "09/06/1958"},
+                new Movie { Id = 29, MovieName = "Black Moon", MovieGenre = "Horror", ReleaseDate = "05/14/1964"},
+                new Movie { Id = 30, MovieName = "Witches of Eastwick", MovieGenre = "Comedy|Fantasy|Horror|Thriller", ReleaseDate = "06/12/1987"}
+                 );
+         }
     }
 }
---
+++ Migrations/20231114150918_SeedData.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MovieDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "Movie");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "Id", "MovieGenre", "MovieName", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "Crime|Mystery|Thriller", "Sherlock Holmes and the Secret Weapon", "09/29/2004" },
+                    { 2, "Animation|Children|Comedy|Romance", "Lady and the Tramp", "09/05/1970" },
+                    { 3, "Documentary", "Collision: Christopher Hitchens vs. Douglas Wilson", "12/04/1987" },
+                    { 4, "Documentary", "Cooking Up Dreams (De ollas y sueños)", "12/14/2013" },
+                    { 5, "Animation", "Merry Madagascar", "04/13/1953" },
+                    { 6, "Comedy|Sci-Fi", "Sleeper", "07/02/2022" },
+                    { 7, "Comedy|Drama", "Article 99", "05/21/1974" },
+                    { 8, "Adventure|Sci-Fi", "Battle in Outer Space", "10/06/1990" },
+                    { 9, "Action|Adventure|Comedy", "The Pirates of Blood River", "05/11/2009" },
+                    { 10, "Comedy", "Fun Size", "06/30/1966" },
+                    { 11, "Drama", "Lenny", "11/19/2011" },
+                    { 12, "Crime|Drama", "Sonny", "06/12/1956" },
+                    { 13, "Thriller", "The Loft", "06/11/2001" },
+                    { 14, "Adventure|Drama|Sci-Fi|Thriller", "Sunshine", "11/16/1978" },
+                    { 15, "Comedy", "One Crazy Summer", "07/15/2000" },
+                    { 16, "Crime|Documentary", "Prophet's Prey", "11/05/1973" },
+                    { 17, "Comedy|Crime|Mystery|Thriller", "Confidentially Yours (Vivement dimanche!)", "03/24/1958" },
+                    { 18, "Action|Adventure|Comedy|Fantasy|Romance", "Librarian: Quest for the Spear", "12/05/2004" },
+                    { 19, "Drama", "The Power and the Glory", "03/30/1950" },
+                    { 20, "Action|Adventure|Comedy", "Lupin the 3rd", "10/08/1966" },
+                    { 21, "Drama|Fantasy|Mystery|Romance", "Dream (Bi-mong)", "07/04/1986" },
+                    { 22, "Drama", "Neds", "02/17/2020" },
+                    { 23, "Drama|Romance|Thriller", "Bodyguard", "11/25/1992" },
+                    { 24, "Comedy", "Austin High", "05/07/2010" },
+                    { 25, "Drama", "Vision (Vision - Aus dem Leben der Hildegard von Bingen)", "10/12/2013" },
+                    { 26, "Crime|Drama", "Reconstruction (Anaparastasi)", "03/19/2022" },
+                    { 27, "Comedy|Romance", "Fever Pitch", "09/10/1973" },
+                    { 28, "Drama|Romance|War", "Holocaust", "09/06/1958" },
+                    { 29, "Horror", "Black Moon", "05/14/1964" },
+                    { 30, "Comedy|Fantasy|Horror|Thriller", "Witches of Eastwick", "06/12/1987" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie");
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.RenameTable(
+                name: "Movie",
+                newName: "Movies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "Id");
+        }
+    }
+}
