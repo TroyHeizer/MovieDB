518ca4965cc826c918490759b8a084928a0ea8bf
troyh <troyheizer@gmail.com>
2023-11-30 09:46:59 -0500

    Seventh commit (bug fix in table, changed port for viewing)

--- obj/Debug/net7.0/MovieDB.AssemblyInfo.cs
+++ obj/Debug/net7.0/MovieDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec231f29c139545267e282615a98b7c3f056b562")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
